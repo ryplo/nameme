78be8b2b03647aefcb996b4d679c1a22dd9f1993
ryplo <lo.rachel8@gmail.com>
2015-10-19 21:36:28 -0400

    practice scraping homepage in allsongsby.com to get the name, artist and album of top songs on charts

--- nameme/src/scraper/Scraper.java
+++ nameme/src/scraper/Scraper.java
@@ -1,6 +1,7 @@
 package scraper;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
@@ -18,13 +19,23 @@ public class Scraper {
 	
 	
 	public static void getHtml() throws IOException {
-		Document doc = Jsoup.connect("http://www.jsoup.org/").get();
-		Elements links = doc.select("a");
-		for(Element e : links)
-		{
-			System.out.println(e.attr("href"));
+		Document doc = Jsoup.connect("http://www.allsongsby.com/").get();
+		String title = doc.title();
+		System.out.println(title);
+		
+		Element table = doc.select("thead").first();
+		Element tr = table.select("tr").first();
+		Elements ths = tr.select("th");
+		Iterator thIt = ths.iterator();
+		
+		for (int i = 0; i < 6; i++) {
+			Element th = (Element) thIt.next();
+			if ( (i == 0) || (i == 4) || (i == 5) )
+			{
+			System.out.print(th.text() + "     ");
+			}
 		}
-			System.out.println("yay!");
+
 	}
 
 }
